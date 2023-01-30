4cf37639ceb9b7052e9c784aa3a161e4063e80a3
husapave <104005005+husapave@users.noreply.github.com>
2023-01-30 14:14:52 +0100

    Update script.js

--- src/script.js
+++ src/script.js
@@ -5,6 +5,6 @@ document.addEventListener('DOMContentLoaded', () => {
     const products1 = document.querySelector('#products-1');
     const productsHeading1 = document.querySelector('.homepage-products-heading-1');
     const benefitBanner = document.querySelector('.benefitBanner');
-    benefitBanner.parentNode.insertBefore(products1, benefitBanner);
     benefitBanner.parentNode.insertBefore(productsHeading1, benefitBanner);
+    benefitBanner.parentNode.insertBefore(products1, benefitBanner);
 });
